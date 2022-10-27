57c4593d3421358f9e0d836f5370d346d13755cb
nosbeto <114036566+nosbeto@users.noreply.github.com>
2022-10-27 14:10:46 -0400

    Update script.js

--- Develop/script.js
+++ Develop/script.js
@@ -9,6 +9,55 @@ var upperCaseR = prompt("Do you want upperCase?", "Y/N");
 var lowerCaseR = prompt("Do you want lowerCase?", "Y/N");
 var symbolsR = prompt("Do you want symbols?", "Y/N");
 
+//// NEW APPROACH ////
+var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var symbols = ["/", "[", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+", "-", "=", "[", "]", "{", "}", ";", ":", "|", ".", "<", ">", "/", "?", "]", "+", "/"]
+var upperCaseLowerCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z","a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var upperCaseSymbols = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z","/", "[", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+", "-", "=", "[", "]", "{", "}", ";", ":", "|", ".", "<", ">", "/", "?", "]", "+", "/"]
+var lowerCaseSymbols =["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z","/", "[", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+", "-", "=", "[", "]", "{", "}", ";", ":", "|", ".", "<", ">", "/", "?", "]", "+", "/"]
+var upperLowerSymbols = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z","a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z","/", "[", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+", "-", "=", "[", "]", "{", "}", ";", ":", "|", ".", "<", ">", "/", "?", "]", "+", "/"]
+
+
+
+superArray = upperCase
+CharactersR = 122
+
+
+const shuffleArray = array => {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    const temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+}
+
+shuffleArray(superArray)
+
+// if user selects "Y" to upperCase or clicks "OK", then add uppercase array to superArray
+if ((upperCaseR === "Y" || upperCaseR === "") && (lowerCaseR === "N" || lowercaseR === null) && (symbolsR === "N" || symbolsR === null)) {
+  superArray = upperCase
+// if user selects "N" OR clicks Cancel, then don't add anything to super Array
+} else if ((lowerCaseR === "Y" || lowerCaseR === "") && (upperCaseR === "N" || upperCaseR === null) && (symbolsR === "N" || symbolsR === null)) {
+  superArray = lowerCase
+// if user selects "N" OR clicks Cancel, then don't add anything to super Array
+} else if ((symbolsR === "Y" || symbolsR === "") && (upperCaseR === "N" || upperCaseR === null) && (lowerCaseR === "N" || lowerCaseR === null)) {
+  superArray = symbols
+// if user selects "N" OR clicks Cancel, then don't add anything to super Array
+} else if ((upperCaseR === "Y" || upperCaseR === "") && (lowerCaseR === "Y" || lowercaseR === "") && (symbolsR === "N" || symbolsR === null)) {
+	superArray = upperCaseLowerCase
+} else if ((upperCaseR === "Y" || upperCaseR === "") && (lowerCaseR === "N" || lowercaseR === null) && (symbolsR === "Y" || symbolsR === "")) {
+	superArray = upperCaseSymbols
+} else if ((lowerCaseR === "Y" || lowerCaseR === "") && (upperCaseR === "N" || upperCaseR === null) && (symbolsR === "Y" || symbolsR === "")) {
+	superArray = lowerCaseSymbols
+} else if ((upperCaseR === "Y" || upperCaseR === "") && (lowerCaseR === "Y" || lowercaseR === "") && (symbolsR === "Y" || symbolsR === "")) {
+	superArray = upperLowerSymbols
+} else {
+  alert("All of your selections were 'N'. we cannot create a password, if you don't make any selections. Stop trying to break my code 🧐")
+}}
+
+////END OF OLD APPROACH
 
 // if user selects "Y" to upperCase or clicks "OK", then add uppercase array to superArray
 if (upperCaseR === "Y" || upperCaseR === "") {
